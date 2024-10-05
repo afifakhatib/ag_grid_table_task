aed1c5133479e2a224c442b41d7f635c34a937fd
afifakhatib <khatibadiba12@gmail.com>
2024-10-05 22:07:11 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GridDataTableComponent } from './shared/components/grid-data-table/grid-data-table.component';
@@ -16,7 +15,6 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     AgGridModule,
     FormsModule
